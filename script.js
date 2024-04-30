c87b3501be0896d011a74dbf58044b1912ed8c38
Giorios <90624324+Giorios@users.noreply.github.com>
2024-04-30 09:50:03 -0400

    attempting to fix cors issue

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-var input = document.getElementById("one")
-var output = document.getElementById("two")
-var translate = document.getElementById("submit")
+var inputText = document.querySelector('#one');
+var outputText = document.querySelector('#two');
+var buttonTranslate = document.querySelector('#translate');
 var apiUrl = "https://api.funtranslations.com/translate/minion.json";
-
+ 
 function errorHandle(error) {    
     alert('Error occurred')
     console.log("error occurred", error);
 }
 
 function clickHandler() {        
-    var text = input.value;
+    var text = inputText.value;
     var updatedUrl = apiUrl + "?text=" + text;
     fetch(updatedUrl).then(response =>
     response.json()).then(json =>
-    output.innerText = 
+    outputText.innerText = 
         (json.contents.translated)).catch(errorHandle);
 }
-
-translate.addEventListener("click", clickHandler);    
+ 
+buttonTranslate.addEventListener("click", clickHandler);    
